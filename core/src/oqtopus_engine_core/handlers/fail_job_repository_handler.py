ddefefd863b33bf380f8d9164e045b39c7919e41
GoralczykS <Slawomir.Goralczyk@fujitsu.com>
2026-02-27 15:23:33 +0100

    feat: updated fail job handler

--- core/src/oqtopus_engine_core/handlers/fail_job_repository_handler.py
+++ core/src/oqtopus_engine_core/handlers/fail_job_repository_handler.py
@@ -13,43 +13,22 @@ logger = logging.getLogger(__name__)
 class FailJobRepositoryHandler(PipelineExceptionHandler):
     """Abstract base class for handling exceptions in the pipeline."""
 
-    async def handle_exception(
+    async def handle_exception(  # noqa: PLR6301
         self,
         ex: Exception,
         gctx: GlobalContext,
-        jctx: JobContext,
+        jctx: JobContext,  # noqa: ARG002
         job: Job,
     ) -> None:
         """Handle an exception raised during pipeline execution."""
-        if jctx.get("has_actual_children", False):
-            # if there are child jobs, update their status
-            await self._update_jobs_status(ex, gctx, job.children)
-        elif jctx.get("has_actual_parent", False):
-            logger.info(
-                "skip repository failure update for internal child job",
+        try:
+            job.status = "failed"
+            job.message = str(ex)
+            await gctx.job_repository.update_job_status(
+                job=job,
+            )
+        except Exception:
+            logger.exception(
+                "failed to update job status to 'failed' in the repository",
                 extra={"job_id": job.job_id, "job_type": job.job_type},
             )
-        else:
-            await self._update_jobs_status(ex, gctx, [job])
-
-    @staticmethod
-    async def _update_jobs_status(
-        ex: Exception,
-        gctx: GlobalContext,
-        jobs: list[Job]
-    ) -> None:
-        """Update the job status to "failed" for the given jobs."""
-        for job in jobs:
-            try:
-                job.status = "failed"
-                job.job_info.result = None  # Clear any partial results
-                job.job_info.message = str(ex)
-                await gctx.job_repository.update_job_info(
-                    job=job,
-                    overwrite_status=job.status,
-                )
-            except Exception:
-                logger.exception(
-                    "failed to update job status to 'failed' in the repository",
-                    extra={"job_id": job.job_id, "job_type": job.job_type},
-                )
