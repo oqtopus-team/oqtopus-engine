97d1781eaac317e550c9f4a258cac875bbfb2911
GoralczykS <Slawomir.Goralczyk@fujitsu.com>
2026-03-02 12:54:33 +0100

    feat: moved job input download to dedicated step

--- core/src/oqtopus_engine_core/steps/job_input_download_step.py
+++ core/src/oqtopus_engine_core/steps/job_input_download_step.py
@@ -1,18 +1,18 @@
 import logging
 
-from oqtopus_engine_core.framework import GlobalContext, Job, JobContext, Step
+from oqtopus_engine_core.framework import GlobalContext, Job, JobContext, JobInput, Step
 
 logger = logging.getLogger(__name__)
 
 
-class JobRepositoryUpdateStep(Step):
-    """Step that updates the job on job repository."""
+class JobInputDownloadStep(Step):
+    """Step that downloads job input data from repository storage."""
 
     def __init__(
         self,
     ) -> None:
         logger.info(
-            "JobRepositoryUpdateStep was initialized",
+            "JobInputDownloadStep was initialized",
         )
 
     async def pre_process(
@@ -23,6 +23,32 @@ class JobRepositoryUpdateStep(Step):
     ) -> None:
         """Pre-process the job.
 
+        This method downloads and extracts job input data from repository storage.
+
+        Args:
+            gctx: The global context.
+            jctx: The job context.
+            job: The job object.
+
+        """
+        input = await gctx.job_repository.download_job_input(
+            job,
+        )
+
+        # use pydantic for final validation
+        validated_input = JobInput(**input)
+
+        job.program = validated_input.program
+        job.operator = validated_input.operator
+
+    async def post_process(  # noqa: PLR6301
+        self,
+        gctx: GlobalContext,
+        jctx: JobContext,  # noqa: ARG002
+        job: Job,
+    ) -> None:
+        """Post-process the job.
+
         Do nothing.
 
         Args:
@@ -31,22 +57,3 @@ class JobRepositoryUpdateStep(Step):
             job: The job object.
 
         """
-
-    async def post_process(  # noqa: PLR6301
-        self,
-        gctx: GlobalContext,
-        jctx: JobContext,  # noqa: ARG002
-        job: Job,
-    ) -> None:
-        """Post-process the job by updating its status in the job repository.
-
-        This method updates the job's status and execution time n the job repository.
-
-        Args:
-            gctx: The global context.
-            jctx: The job context.
-            job: The job object.
-
-        """
-        job.status = "succeeded"
-        await gctx.job_repository.update_job_status_nowait(job)
