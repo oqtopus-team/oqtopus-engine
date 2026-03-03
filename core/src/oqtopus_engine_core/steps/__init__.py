8850c95ccc4cec671402682b4f32f1cfac8cd919
GoralczykS <Slawomir.Goralczyk@fujitsu.com>
2026-03-03 16:51:53 +0100

    feat: removed job input download step from pipeline

--- core/src/oqtopus_engine_core/steps/__init__.py
+++ core/src/oqtopus_engine_core/steps/__init__.py
@@ -1,7 +1,6 @@
 from .debug_step import DebugStep
 from .device_gateway_step import DeviceGatewayStep
 from .estimator_step import EstimatorStep
-from .job_input_download_step import JobInputDownloadStep
 from .job_repository_update_step import JobRepositoryUpdateStep
 from .multi_manual_step import MultiManualStep
 from .ro_error_mitigation_step import ReadoutErrorMitigationStep
@@ -12,7 +11,6 @@ __all__ = [
     "DebugStep",
     "DeviceGatewayStep",
     "EstimatorStep",
-    "JobInputDownloadStep",
     "JobRepositoryUpdateStep",
     "MultiManualStep",
     "ReadoutErrorMitigationStep",
