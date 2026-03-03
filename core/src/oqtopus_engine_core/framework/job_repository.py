2bccab80269f292459359eb1fc30809a4d1609a7
GoralczykS <Slawomir.Goralczyk@fujitsu.com>
2026-03-03 16:39:29 +0100

    fead: added missing function definitions to JobRepository interface

--- core/src/oqtopus_engine_core/framework/job_repository.py
+++ core/src/oqtopus_engine_core/framework/job_repository.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 from .model import Job
 from ..interfaces.oqtopus_cloud import JobsJobInfoUploadPresignedURL
@@ -117,5 +118,50 @@ class JobRepository(ABC):
         message = (
             "`update_job_transpiler_info_nowait` must be implemented in subclasses of "
             "JobUpdater."
+        )
+        raise NotImplementedError(message)
+
+    @abstractmethod
+    async def download_job_input(
+        self,
+        job: Job,
+    ) -> dict[str, Any]:
+        """Downloads and extracts job input .zip file form job data storage
+
+        Args:
+            job: The job for input download.
+
+        Returns:
+            A dictionary containing downloaded and extracted job input items.
+
+        Raises:
+            NotImplementedError: If not implemented in subclass.
+
+        """
+        message = (
+            "`download_job_input` must be implemented in subclasses of JobRepository."
+        )
+        raise NotImplementedError(message)
+
+    @abstractmethod
+    async def upload_job_output(
+        self,
+        job: Job,
+        presigned_url: JobsJobInfoUploadPresignedURL,
+        data: dict[str, Any],
+    ) -> None:
+        """Uploads job output data as .zip file to job data storage
+
+        Args:
+            job: The job for output upload.
+            presigned_url: Presigned URL for upload.
+            data: Data to be uploaded.
+
+        Raises:
+            NotImplementedError: If not implemented in subclass.
+
+        """
+        message = (
+            "`upload_job_output` must be implemented in subclasses of JobRepository."
         )
         raise NotImplementedError(message)
