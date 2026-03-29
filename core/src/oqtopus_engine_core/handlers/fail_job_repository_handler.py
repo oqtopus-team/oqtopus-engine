9fa1d0f2ed74e799cb07684c76ac13506cef252d
SECuchida <141290996+SECuchida@users.noreply.github.com>
2026-03-30 03:54:14 +0900

    feat: [multi-programming auto] add buffer and step for mp-auto into core pipeline (#36)
    * feat: Add multi-programming auto combining
    
    * feat: update the interface of combiner
    
    * feat: update default config value of multi-programming auto
    
    * refactor: update class/function comments
    
    * feat:Update to support a framework change regarding splitting job and some refactoring
    
    * feat: Use split_skip_steps instead of split_enabled_steps to cancel splitting in MpAutoCombining
    
    * refactor: Change the timing to reverse the order of qubits list for multi-programming
    
    * refactor: refactor codes and comments in mp auto combining

--- core/src/oqtopus_engine_core/handlers/fail_job_repository_handler.py
+++ core/src/oqtopus_engine_core/handlers/fail_job_repository_handler.py
@@ -13,24 +13,38 @@ logger = logging.getLogger(__name__)
 class FailJobRepositoryHandler(PipelineExceptionHandler):
     """Abstract base class for handling exceptions in the pipeline."""
 
-    async def handle_exception(  # noqa: PLR6301
+    async def handle_exception(
         self,
         ex: Exception,
         gctx: GlobalContext,
-        jctx: JobContext,  # noqa: ARG002
+        jctx: JobContext,
         job: Job,
     ) -> None:
         """Handle an exception raised during pipeline execution."""
-        try:
-            job.status = "failed"
-            job.job_info.result = None  # Clear any partial results
-            job.job_info.message = str(ex)
-            await gctx.job_repository.update_job_info(
-                job=job,
-                overwrite_status=job.status,
-            )
-        except Exception:
-            logger.exception(
-                "failed to update job status to 'failed' in the repository",
-                extra={"job_id": job.job_id, "job_type": job.job_type},
-            )
+        if jctx.get("has_actual_children", False):
+            # if there are child jobs, update their status
+            await self._update_jobs_status(ex, gctx, job.children)
+        else:
+            await self._update_jobs_status(ex, gctx, [job])
+
+    @staticmethod
+    async def _update_jobs_status(
+        ex: Exception,
+        gctx: GlobalContext,
+        jobs: list[Job]
+    ) -> None:
+        """Update the job status to "failed" for the given jobs."""
+        for job in jobs:
+            try:
+                job.status = "failed"
+                job.job_info.result = None  # Clear any partial results
+                job.job_info.message = str(ex)
+                await gctx.job_repository.update_job_info(
+                    job=job,
+                    overwrite_status=job.status,
+                )
+            except Exception:
+                logger.exception(
+                    "failed to update job status to 'failed' in the repository",
+                    extra={"job_id": job.job_id, "job_type": job.job_type},
+                )
---
+++ core/src/oqtopus_engine_core/mp/auto_combining/__init__.py
@@ -0,0 +1,7 @@
+from .mp_auto_combining_buffer import MpAutoCombiningBuffer
+from .mp_auto_combining_step import MpAutoCombiningStep
+
+__all__ = [
+    "MpAutoCombiningBuffer",
+    "MpAutoCombiningStep",
+]
