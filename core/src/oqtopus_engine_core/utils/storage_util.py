64cbb0b58700d907ae861f718958fe26661ea642
snuffkin <tknstyk@gmail.com>
2026-08-27 14:32:06 +0900

    style: apply ruff formatting to storage_util.py

--- core/src/oqtopus_engine_core/utils/storage_util.py
+++ core/src/oqtopus_engine_core/utils/storage_util.py
@@ -119,8 +119,7 @@ class OqtopusStorage:
                 # and changes fields names e.g. AWSAccessKeyId -> aws_access_key_id
                 # we get the true field names
                 original_fields = {
-                    fields.attribute_map[k]: v
-                    for (k, v) in fields.to_dict().items()
+                    fields.attribute_map[k]: v for (k, v) in fields.to_dict().items()
                 }
                 storage_key = str(fields.key)
 
